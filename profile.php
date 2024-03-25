a5940723427d88ac4a656ac1a40baf9f8e31b2ba
Aron717 <pinterroniaron@gmail.com>
2024-03-25 18:38:37 +0100

    Profile is deletable

--- profile.php
+++ profile.php
@@ -21,7 +21,11 @@ $query = mysqli_query($conn, "SELECT * FROM users WHERE usersId = " . mysqli_rea
         while ($rowImg = mysqli_fetch_assoc($resultImg)) {
             echo "<div>";
                 if ($rowImg["status"] == 0) {
-                    $pfp = 'data/profile'.$id.'.jpg?'.mt_rand();
+                    $filename = "data/profile".$id."*";
+                    $fileinfo = glob($filename);
+                    $fileExt = explode(".", $fileinfo[0]);
+                    $fileactualext = $fileExt[1];
+                    $pfp = 'data/profile'.$id.'.'.$fileactualext.'?'.mt_rand();
                 }
                 else {
                     $pfp = 'images/nopfp.png';
@@ -38,13 +42,17 @@ $query = mysqli_query($conn, "SELECT * FROM users WHERE usersId = " . mysqli_rea
     $expected_url = "/MusicSocialMedia/profile.php";
     $expected_url2 = "/MusicSocialMedia/profile.php?error=wrongfile";
     $expected_url3 = "/MusicSocialMedia/profile.php?error=successful";
+    $expected_url4 = "/MusicSocialMedia/profile.php?error=deleted";
+    $expected_url5 = "/MusicSocialMedia/profile.php?error=notdeleted";
     $current_url = $_SERVER["REQUEST_URI"];
-    if ($current_url == $expected_url OR $current_url == $expected_url2 OR $current_url == $expected_url3) {
-        echo  "<img src='$pfp' style='background-color: rgba(119,79,197,0.57); width: 200px; height: 200px; border-radius: 50%; margin-top: 30px'>
+    if ($current_url == $expected_url OR $current_url == $expected_url2 OR $current_url == $expected_url3 OR $current_url == $expected_url4 OR $current_url == $expected_url5) {
+        echo  "<img src='$pfp' style='background-color: rgba(119,79,197,0.57); width: 200px; height: 200px; border-radius: 50%; margin-top: 30px; object-fit: cover'>
     <form action='includes/imgupload.inc.php' method='post' enctype='multipart/form-data' style='margin-top: 20px'>
         Select image to upload:
         <input type='file' name='file' style='width: 200px'><br>
         <button type='submit' name='submit' style='border-radius: 20px'>Upload</button></form>";
+        echo  "<form action='includes/deletepfp.inc.php' method='post' enctype='multipart/form-data' style='margin-top: 20px'>
+        <button type='submit' name='submit' style='border-radius: 20px'>Delete Profile Image</button></form>";
     } else {
         echo "<img src='$pfp' style='background-color: rgba(119,79,197,0.57); width: 200px; height: 200px; border-radius: 50%; margin-top: 30px; object-fit: cover'>";
         echo "<button style='margin-top: 20px; border-radius: 20px; background-color: #8705f1; color: white'>Add as a friend</button>";
@@ -59,6 +67,12 @@ $query = mysqli_query($conn, "SELECT * FROM users WHERE usersId = " . mysqli_rea
             }
             elseif ($_GET["error"] == "wrongfile") {
                 echo "<p style='color: white; font-weight: bold'>The image is not in the correct file extension!</p>";
+            }
+            elseif ($_GET["error"] == "deleted") {
+                echo "<p style='color: white; font-weight: bold'>Successfully deleted!</p>";
+            }
+            elseif ($_GET["error"] == "notdeleted") {
+                echo "<p style='color: white; font-weight: bold'>Couldn't delete it!</p>";
             }
         }
         function addFriend($newId, $friendid) {
